7b04c2eca272c1ecf772fe997c415cd5edc4df2e
Bbosa Muhamood <bmuhamood@edch.com>
2025-09-24 11:03:06 +0400

    fixed allowed Hosts and Debug

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^a69t31k1l$yakx*1qq_)%8x!338t5&nw(8gjvm43r5ad7$@e7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["globishr.onrender.com"]
 
 # Application definition
 
